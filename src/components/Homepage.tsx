6e9af19bc4906c558736e65a518e7b52f5b5b025
RereTooki <rereloluwa.afolabi.tooki@gmail.com>
2025-04-26 18:56:55 +0100

    First Commit for AX Partners

---
+++ src/components/Homepage.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../App.css";
+
+const Homepage = () => {
+  return (
+    <>
+      <h1
+        className="text-3xl exoFont font-bold text-red-500 underline text-center"
+        data-aos="fade-up"
+        data-aos-duration="1200"
+      >
+        {" "}
+        AX Partners
+      </h1>{" "}
+    </>
+  );
+};
+
+export default Homepage;
